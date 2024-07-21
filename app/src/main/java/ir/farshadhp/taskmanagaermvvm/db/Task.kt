aec9827a29ccab5ec3da2cf42c7d3d32dc286d55
farshadHP <haftzendegi@gmail.com>
2024-07-21 19:04:19 +0330

    add Task Data class

---
+++ app/src/main/java/ir/farshadhp/taskmanagaermvvm/db/Task.kt
@@ -0,0 +1,14 @@
+package ir.farshadhp.taskmanagaermvvm.db
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    val title: String,
+    val time: String,
+    val isComplete: Boolean = false,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+)
